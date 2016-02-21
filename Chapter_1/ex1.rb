28cfeb8254bcbbf7aa642c680a00c1bdf9fdcaeb
Laurie Linz <lvlinz@comcast.net>
2016-02-21 11:10:50 -0700

    "Completed homework and added Chapter_1 Readme.md and ex1.rb"
    [#113796093]

---
+++ Chapter_1/ex1.rb
@@ -0,0 +1,25 @@
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts
+puts 'did it print another line above?'
+
+# puts 'Hello World!'
+# puts 'Hello Again'
+# puts 'I like typing this.'
+puts 'This is fun.'
+# puts 'Yay! Printing'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
