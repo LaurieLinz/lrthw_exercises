25009fb39e906b1bff822dc975e5a115709c2ac8
Laurie Linz <lvlinz@comcast.net>
2016-02-29 13:27:42 -0700

    Completed Chapter 5
    [#113796101]

---
+++ Chapter_5/ex5.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+cm = 2.54
+height = 74 * cm # cm
+kg = 2.2
+weight = 180 / kg # kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight}
+I get #{age + height + weight}."
