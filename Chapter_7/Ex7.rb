bd88439ce5bbd9a1a6a28cf410f6ff08095c42b7
Laurie Linz <lvlinz@comcast.net>
2016-03-02 17:57:20 -0700

    Completed Chapter 7
    [#113796111]

---
+++ Chapter_7/Ex7.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# puts phrase
+puts 'Mary had a little lamb.'
+# A very complicated way of writing this sentence by placing the
+# string 'snow' inside interpolation.
+snow = 'snow'
+puts "It's fleece was white as #{snow}."
+# Just a string
+puts 'And everywhere Mary went.'
+# place 10 periods
+puts '.' * 10 # place 10 periods
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# this will execute just before the programs ends
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
---
+++ Chapter_7/ReadmeEx7.md
@@ -0,0 +1,7 @@
+Rubocop did not like literal interpolation in #{'snow'} so I removed the single quotes and made snow a variable.
+
+I also had to correct line length.
+
+Puts literally means "Print String" and places an empty line at the end. Print simply prints out the information.
+
+
