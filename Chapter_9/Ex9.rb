c32fe9bd9c6f444d29b928d0df0b9827a3e865dc
Laurie Linz <lvlinz@comcast.net>
2016-03-22 14:44:04 -0600

    Made changes per request
    [#113796123]

--- Chapter_9/Ex9.rb
+++ Chapter_9/Ex9.rb
@@ -11,3 +11,15 @@ puts "Here are the months: #{months}"
 puts "There's something going on here.\nWith the three double-quotes."
 puts 'We will be able to type as much as we like.'
 puts 'Even 4 lines if we want, or 5 or 6.'
+
+#It does not work without the tree puts statements but this will work
+puts "There's something going on here.\nWith the three double-quotes.
+We will be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6."
+
+#This works as well
+puts <<HEREDOC
+"There's something going on here.\nWith the three double-quotes.
+We will be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6."
+HEREDOC
